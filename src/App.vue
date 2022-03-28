3f826a48412f56f86e8ec3dc26528b077de67355
Nikola M <nikola.merlic2@gmail.com>
2022-03-28 14:12:58 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,48 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+  <nav id="nav" class="navbar-expand-lg navbar-light bg-light">
+
+  <a class="navbar-brand" href="#">
+    <img src="@/assets/tfpu.png" height="40" 
+    class="d-inline-block align-top" alt="" loading="lazy">
+  </a>
+
+  <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarToggler"
+        aria-controls="navbarToggler"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+  </button>
+      <div class="collapse navbar-collapse" id="navbarToggler">
+
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+        <li class="nav-item">
+          <router-link to="/" class="nav-link">Home</router-link>
+        </li>
+        <li class="nav-item"> 
+      <router-link to="/login" class="nav-link">Prijava</router-link>
+        </li>
+      <li class="nav-item">  
+      <router-link to="/signup" class="nav-link">Registracija</router-link>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search"
+       placeholder="Pretraga" aria-label="Search" />
+    </form>
+    </div>
+  </nav>
+
+<div class="container">
+  <router-view />
+</div>
   </div>
+
 </template>
 
 <style lang="scss">
--- src/router/index.js
+++ src/router/index.js
@@ -11,19 +11,28 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/login',
+    name: 'Login',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    component: () => import(/* webpackChunkName: "Login" */ '../views/Login.vue')
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "Signup" */ '../views/Signup.vue')
+  },
+];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+},
+);
 
 export default router
